a36686598087a467468e2c9a0ea9777ce40ce223
Shreyajadhav088 <shreyajadhav2402@gmail.com>
2025-06-25 19:55:53 +0530

    Add files via upload

---
+++ squareptr.java
@@ -0,0 +1,22 @@
+public class squareptr {
+    public static void main(String[] args) {
+ 
+int rows=4;
+int column=4;
+ for(int i=0; i < rows; i++)
+{
+
+ for(int j=0; j < column; j++){
+
+    System.out.print("*");
+ }
+System.out.println();
+
+
+}
+
+
+
+
+}
+}
