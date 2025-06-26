e15f0dd137fe5944f02917035e53bd40a5023af4
Shreyajadhav088 <shreyajadhav2402@gmail.com>
2025-06-26 20:47:44 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,21 @@
+public class Evenodd{
+
+public static void main(String args[]){
+
+
+int num=89;
+if(num%2==0){
+System.out.println("number " +num+  " is Even");
+}
+
+else{
+
+
+    System.out.println("number  "+num+  " is odd");
+}
+}
+
+
+
+
+}
