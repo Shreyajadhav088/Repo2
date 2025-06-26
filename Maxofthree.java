c81104601f00fdd50e67bc73cb55a3283418f838
Shreyajadhav088 <shreyajadhav2402@gmail.com>
2025-06-26 20:48:26 +0530

    Add files via upload

---
+++ Maxofthree.java
@@ -0,0 +1,17 @@
+public class Maxofthree {
+    public static void main(String args[])
+    {
+int a=10;
+int b=20;
+int c=30;
+
+int max=a;
+
+if(b>max)max=b;
+if(c>max)max=c;
+System.out.println ("Maximum is" +max);
+
+}
+
+
+    }
